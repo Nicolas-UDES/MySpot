8aaf50e24064bb3c152369c8c40e6a6883710359
DESKTOP-7AIVUAH\Squirrel <lamn2005@usherbrooke.ca>
2017-11-24 19:04:05 -0500

    More entities and started adding the profile activity.

--- app/src/main/java/com/ift604/udes/myspot/Android/MapsActivity.java
+++ app/src/main/java/com/ift604/udes/myspot/Android/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.ift604.udes.myspot;
+package com.ift604.udes.myspot.Android;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -9,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -21,23 +22,30 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.ift604.udes.myspot.Entites.Enumerable.TerritoryType;
 import com.ift604.udes.myspot.Entites.Territory;
+import com.ift604.udes.myspot.R;
 import com.ift604.udes.myspot.Utility.Delayer;
 
 import java.lang.reflect.Type;
+import java.util.Dictionary;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.security.AccessController.getContext;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final int MY_LOCATION_REQUEST_CODE = 245;
+    private HashMap<Territory, PolygonOptions> territories;
     private GoogleMap myMap;
 
     @Override
@@ -50,58 +58,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         myMap = googleMap;
 
-        if(askForFineLocation()) {
-            Delayer.delay(1000, new Runnable() {
-                @Override
-                public void run() {
-                    setCamera();
-                    fetchNearTerritories();
-                }
-            });
+        setCamera();
+        if(territories != null){
+            showTerritories();
+        }
+        else {
+            fetchNearTerritories();
         }
     }
 
     private void setCamera() {
-        CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(45.38072, -71.92613));
-        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+        CameraUpdate center = CameraUpdateFactory.newLatLngZoom(new LatLng(45.38072, -71.92613), 15);
         myMap.moveCamera(center);
-        myMap.animateCamera(zoom);
     }
 
     private boolean askForFineLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             myMap.setMyLocationEnabled(true);
             return true;
-        } else {
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
-
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
-            }
-
         }
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_LOCATION_REQUEST_CODE);
         return false;
     }
 
@@ -149,19 +130,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         Volley.newRequestQueue(this).add(stringRequest);
     }
 
-    private void showTerritories(List<Territory> territories) {
-        for(Territory territory : territories){
-            PolygonOptions polygonOptions = new PolygonOptions();
-            polygonOptions.addAll(territory.getPositions());
-            polygonOptions.strokeColor(Color.BLACK);
-            polygonOptions.strokeWidth(5.0f);
-            polygonOptions.fillColor(Color.argb(30, 0, 200, 0));
-
-            Polygon polygon = myMap.addPolygon(polygonOptions);
+    private void showTerritories(List<Territory> territoryList) {
+        territories = new HashMap<>();
+        for(Territory territory : territoryList){
+            PolygonOptions polygon = getPolygonOptions(territory);
+            myMap.addPolygon(polygon);
+            territories.put(territory, polygon);
         }
     }
 
-    private void gettingNearTerritoriesError(VolleyError error) {
+    private void showTerritories() {
+        territories = new HashMap<>();
+        for(PolygonOptions territory : territories.values()){
+            myMap.addPolygon(territory);
+        }
+    }
 
+    private PolygonOptions getPolygonOptions(Territory territory) {
+        PolygonOptions polygonOptions = new PolygonOptions();
+        polygonOptions.addAll(territory.getPositions());
+        polygonOptions.strokeColor(Color.BLACK);
+        polygonOptions.strokeWidth(5.0f);
+        polygonOptions.fillColor(getColor(territory.getTerritoryType()));
+
+        return polygonOptions;
+    }
+
+    private void gettingNearTerritoriesError(VolleyError error) {
+        Log.e("gettingNearTerritoriesError", error.getMessage());
+    }
+
+    private int getColor(TerritoryType type){
+        final int alpha = 30;
+        switch (type){
+            case Water:
+                return Color.argb(alpha, 0, 0, 200);
+            case Gainable:
+                return Color.argb(alpha, 0, 200, 0);
+            default:
+                return Color.argb(alpha, 200, 0, 0);
+        }
     }
 }
---
+++ app/src/main/java/com/ift604/udes/myspot/Entites/Mark.java
@@ -0,0 +1,56 @@
+package com.ift604.udes.myspot.Entites;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Mark implements Serializable {
+	private int id;
+
+	private Date date;
+
+	private double amount;
+
+	private double strength;
+
+	public Mark() {
+	}
+
+	public Mark(int id, Date date, double amount, double strength) {
+		this.id = id;
+		this.date = date;
+		this.amount = amount;
+		this.strength = strength;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getStrength() {
+		return strength;
+	}
+
+	public void setStrength(double strength) {
+		this.strength = strength;
+	}
+}
